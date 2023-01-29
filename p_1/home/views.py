aa4c9e1a613d4b021746dd0f27657e7078ff3f8a
ashokcu62 <ashokcu62@gmail.com>
2023-01-29 18:55:25 +0530

    variable share and render

--- p_1/home/views.py
+++ p_1/home/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    person={
+        "name":"Archana",
+        "age":14,
+        "place":"mannarkkad"
+    }
+    return render(request,'index.html',person)
 
 def about(request):
     return render(request,"about.html")
