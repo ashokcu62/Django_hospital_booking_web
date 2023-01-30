ceec705ecc08213846be9f95f50ec2c76b4511ec
ashokcu62 <ashokcu62@gmail.com>
2023-01-30 09:39:52 +0530

    liks were add by path fn name arg and base html hrref urltag

--- p_1/home/urls.py
+++ p_1/home/urls.py
@@ -2,11 +2,12 @@ from django.urls import path , include
 from . import views
 
 urlpatterns = [
-    path("",views.index),
-    path("about/",views.about),
-    path("doctors/",views.doctors),
-    path("booking/",views.booking),
-    path("contact/",views.contact),
-    path("department/",views.department)
+    
+    path("",views.index,name='home'),
+    path("about/",views.about,name='about'),
+    path("doctors/",views.doctors,name='doctors'),
+    path("booking/",views.booking,name='booking'),
+    path("contact/",views.contact,name="contact"),
+    path("department/",views.department,name="department")
 
 ]
